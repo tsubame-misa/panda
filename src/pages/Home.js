5ab3692bdad5b3a49b16135b99a57d0b99b3c870
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2021-01-14 02:09:38 +0900

    css changed home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,41 +12,51 @@ import panda from "../img/panda_touka.png";
 const Home = ({ history }) => {
   return (
     <IonPage>
-      <IonContent fullscreen className="center">
-        <IonItem className="title" lines="none">
-          <IonLabel>パンダの館</IonLabel>
-        </IonItem>
+      <IonContent fullscreen color="dark">
+        <div className="display-flex">
+          <div className="my-2 py-24 bg-white">
+            <IonItem
+              lines="none"
+              className="mt-3 text-center text-5xl font-semibold"
+            >
+              <IonLabel className="">パンダの館</IonLabel>
+            </IonItem>
 
-        <img className="panda" src={panda}></img>
+            <img className="object-contain h-48 w-full " src={panda}></img>
 
-        <IonButton
-          className="button1"
-          fill="outline"
-          color="dark"
-          onClick={() => history.push("/talk")}
-        >
-          パンダとおしゃべり
-        </IonButton>
-        {/*<a class="button is-warning" href="/talk">全てのラーメン店を見る</a>*/}
-        <br />
-        <IonButton
-          className="button2"
-          fill="outline"
-          color="dark"
-          onClick={() => history.push("/teach")}
-        >
-          パンダ構文講座
-          {/*} <IonItem href="/teach"></IonItem>*/}
-        </IonButton>
+            <div className="mt-8 text-center">
+              <IonButton
+                className="justify-self-center "
+                fill="outline"
+                color="dark"
+                onClick={() => history.push("/talk")}
+              >
+                パンダとおしゃべり
+              </IonButton>
+              {/*<a class="button is-warning" href="/talk">全てのラーメン店を見る</a>*/}
+            </div>
+            <div className="">
+              <IonButton
+                className="button2"
+                fill="outline"
+                color="dark"
+                onClick={() => history.push("/teach")}
+              >
+                パンダ構文講座
+                {/*} <IonItem href="/teach"></IonItem>*/}
+              </IonButton>
 
-        <IonButton
-          className="button3"
-          fill="outline"
-          color="dark"
-          onClick={() => history.push("/knife")}
-        >
-          パンダに刺される
-        </IonButton>
+              <IonButton
+                className="button3"
+                fill="outline"
+                color="dark"
+                onClick={() => history.push("/knife")}
+              >
+                パンダに刺される
+              </IonButton>
+            </div>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
